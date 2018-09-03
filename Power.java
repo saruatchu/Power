3fd4568086640b825f2030131ee88cae5bdc8547
saruatchu <sarumathiayyappan@gmail.com>
2018-09-03 12:39:24 +0530

    Create Power.java

---
+++ Power.java
@@ -0,0 +1,15 @@
+# Power
+import java.io.*;
+import java.util.*;
+class Power
+{
+public static void main (String args[]0
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter value");
+int value=sc.nextInt();
+System.out.println("enter power");
+int power=sc.nextInt();
+System.out.println("output"+(value^power));
+}
+}
